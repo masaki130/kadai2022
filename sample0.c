e041536ce1858012aba099817aa202b17f9a16e0
Masaki Mitani <pasapasamasaki0@gmail.com>
2022-12-10 04:23:01 +0900

    make ensyuuritu

--- sample0.c
+++ sample0.c
@@ -6,22 +6,25 @@
 
 int main(void) {
 int i, j;
-int Max = 10;
+int Max = 10000000;
 int count = 0;
-double x, y, k;
+double x, y, k, g;
 	srand((unsigned int)time(NULL));
 	/*i = rand() % 10 + 1;*/
 	for(j = 0; j < Max; j++) {
 		x = (double)rand() / RAND_MAX;
 		y = (double)rand() / RAND_MAX;
 		if((x-0.5) * (x-0.5) + (y-0.5) * (y-0.5) <= 0.5 * 0.5) {
-			/*printf("(x,y)=(%.1lf,%.1lf)\n", x, y);*/
+			/*printf("(x,y)=(%.3lf,%.3lf)\n", x, y);*/
 			count += 1;
 
 		}
 	}
 	/*printf("点が円の内側に入る回数：%d\n", count);*/
 	k = (double)count / (double)Max;	/*k:円の面積*/
-	printf("%.1lf\n", k);
+	/*printf("%.3lf\n", k);*/
+	g = 4 * k;
+        printf("%.20lf\n", g);	/*g:円周率*/
+
 return 0;
 }
