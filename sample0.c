03f7ad4c05070eec2dd6f02e64b4f3c461050a90
Masaki Mitani <pasapasamasaki0@gmail.com>
2022-12-10 03:57:11 +0900

    area of a circle

--- sample0.c
+++ sample0.c
@@ -5,13 +5,23 @@
 #include <time.h>
 
 int main(void) {
-int i, j, k;
-double x, y;
+int i, j;
+int Max = 10;
+int count = 0;
+double x, y, k;
 	srand((unsigned int)time(NULL));
 	/*i = rand() % 10 + 1;*/
-	x = (double)rand() / RAND_MAX;
-	y = (double)rand() / RAND_MAX;
-	printf("x成分：%.1lf\n", x);
-	printf("y成分：%.1lf\n", y);
+	for(j = 0; j < Max; j++) {
+		x = (double)rand() / RAND_MAX;
+		y = (double)rand() / RAND_MAX;
+		if((x-0.5) * (x-0.5) + (y-0.5) * (y-0.5) <= 0.5 * 0.5) {
+			/*printf("(x,y)=(%.1lf,%.1lf)\n", x, y);*/
+			count += 1;
+
+		}
+	}
+	/*printf("点が円の内側に入る回数：%d\n", count);*/
+	k = (double)count / (double)Max;	/*k:円の面積*/
+	printf("%.1lf\n", k);
 return 0;
 }
